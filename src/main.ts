3f7389e3e03f8b0a68ee73aad1e26b37fdc77e81
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2025-06-15 13:09:35 +0200

    Initial state

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,58 @@
-import { createGame } from 'odyc'
+import { createGame } from "odyc";
 
-const game = createGame({
-  title: 'My Awesome Game'
+createGame({
+  player: {
+    sprite: 3,
+    position: [0, 0],
+  },
+  templates: {
+    g: {
+      sprite: 7,
+      solid: false,
+    },
+    a: {
+      sprite: 4
+    }
+  },
+  map: `
+		ggaggggg
+		gggggggg
+		gggggggg
+		gggggggg
+		gggggggg
+		gggggggg
+		gggggggg
+		gggggggg
+	`,
+  colors: [
+    '#212529',
+    '#f8f9fa',
+    '#ced4da',
+    '#228be6',
+    '#fa5252',
+    '#fcc419',
+    '#ff922b',
+    '#40c057',
+    '#f06595',
+    '#a52f01'
+  ],
+  messageBackground: 0,
+  messageColor: 1,
+  dialogBackground: 0,
+  dialogColor: 1,
+  dialogBorder: 1,
+  dialogInternvalMs: 30,
+  screenWidth: 8,
+  screenHeight: 8,
+  cellWidth: 8,
+  cellHeight: 8,
+  background: 1,
+  volume: 0.5,
+  controls: {
+    LEFT: 'ArrowLeft',
+    RIGHT: 'ArrowRight',
+    UP: 'ArrowUp',
+    DOWN: 'ArrowDown',
+    ACTION: ['Enter', 'Space']
+  }
 })
